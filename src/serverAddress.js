346f04d53aba05d86f54b209e80939e4cfbfddc6
Lennedy <lennedy.soares@ifrn.edu.br>
2026-05-15 21:28:24 -0300

    Adicionado o server address para futuras conexões

---
+++ src/serverAddress.js
@@ -0,0 +1,10 @@
+// endereço do servidor onde está a API
+export default function getApiAddress() {
+  // const apiAddress = { database: "https://10.34.9.178/api", serial: "localhost" };
+  const apiAddress = { database: "http://127.0.0.1:5000", serial: "http://127.0.0.1:8000" };
+  return apiAddress;
+}
+
+// const apiAddress = { api: "https://192.168.1.29/api", serial: "localhost" };
+
+// export default apiAddress;
